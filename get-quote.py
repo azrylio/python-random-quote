4f78221e4e3d21e57b87f3c9bc523ba54be690cd
azrylio <anatolik_azry@mail.ru>
2021-05-11 19:54:53 +0300

    added some new lines: new quotes manyally added to quotes.txt; /n remowed when print called; more than one quote pronting when app runed

--- get-quote.py
+++ get-quote.py
@@ -5,12 +5,13 @@ def main():
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = f.read().splitlines()
   f.close()
 
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-
+  print(quotes[rnd])
+  rnd = random.randint(0, last)
   print(quotes[rnd])
 
 if __name__== "__main__":
